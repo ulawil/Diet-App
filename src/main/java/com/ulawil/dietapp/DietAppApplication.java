ee8c7d037011e6441bf3c358413993f5a3f548e2
ulawil <ulawil97@gmail.com>
2022-07-12 03:58:43 +0200

    minor cosmetic view fix + made email sending async

--- src/main/java/com/ulawil/dietapp/DietAppApplication.java
+++ src/main/java/com/ulawil/dietapp/DietAppApplication.java
@@ -2,8 +2,10 @@ package com.ulawil.dietapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class DietAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DietAppApplication.class, args);
